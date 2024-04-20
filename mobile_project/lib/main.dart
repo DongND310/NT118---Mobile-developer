342e4e8b077bee745f6fcb16b6df5f545c0d9944
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-04-21 03:18:09 +0700

    update

--- mobile_project/lib/main.dart
+++ mobile_project/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_project/screen/welcome.dart';
+import 'package:mobile_project/screen/login-regis/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
