fa1cd83340599b198178062e34aa9fd93e37bb70
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-04-21 03:27:45 +0700

    update BE login

---
+++ mobile_project/lib/screen/login-regis/authpage.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_project/screen/login-regis/welcome.dart';
+import '../homepage/homepage.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // is logged
+          if (snapshot.hasData) {
+            return HomePageScreen();
+          }
+
+          // not logged
+          else {
+            return WelcomeScreen();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ mobile_project/lib/screen/login-regis/login.dart
@@ -0,0 +1,299 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile_project/components/inputtext.dart';
+import 'package:flutter/material.dart';
+import 'forgotpass.dart';
+import 'signup.dart';
+import 'welcome.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signUserIn() async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(
+              child: CircularProgressIndicator(
+            color: Colors.blue,
+          ));
+        });
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      if (e.code == 'user-not-found') {
+        wrongEmailMessage();
+      } else if (e.code == 'wrong-password') {
+        wrongPassWordMessage();
+      }
+    }
+  }
+
+  void wrongEmailMessage() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            title: Text('Tài khoản Email không tồn tại.'),
+          );
+        });
+  }
+
+  void wrongPassWordMessage() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            title: Text('Sai mật khẩu, vui lòng nhập lại mật khẩu.'),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => WelcomeScreen()));
+          },
+          icon: SvgPicture.asset(
+            'assets/icons/ep_back.svg',
+            width: 30,
+            height: 30,
+          ),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: ListView(
+            padding: const EdgeInsets.only(top: 0.0, left: 40, right: 45),
+            children: [
+              Text(
+                'Chào mừng!',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              Text(
+                'Đăng nhập để tiếp tục',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 0.0),
+                child: Container(
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      //Email, password
+                      MyTextField(
+                          controller: emailController,
+                          label: "Email",
+                          hint: "Nhập email người dùng",
+                          obscureText: false),
+                      MyTextField(
+                          controller: passwordController,
+                          label: "Password",
+                          hint: "Nhập mật khẩu",
+                          obscureText: true),
+
+                      SizedBox(
+                        height: 5,
+                      ),
+
+                      //remember - forgot pass
+                      Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                child: Row(
+                                  children: [
+                                    Checkbox(
+                                      value: false,
+                                      onChanged: (newValue) {
+                                        print('New value: $newValue');
+                                      },
+                                      activeColor: Colors.blue,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(45)),
+                                      // visualDensity: VisualDensity.compact,
+                                    ),
+                                    Text(
+                                      'Nhớ mật khẩu?',
+                                      style: TextStyle(
+                                          color: Colors.black87, fontSize: 13),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ForgotPassScreen()));
+                                },
+                                child: Text(
+                                  'Quên mật khẩu?',
+                                  style: TextStyle(
+                                    color: Colors.black87,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              )
+                            ]),
+                      ),
+                      SizedBox(height: 10),
+
+                      // login button
+                      GestureDetector(
+                        onTap: signUserIn,
+                        child: Container(
+                          height: 55,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'ĐĂNG NHẬP',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  letterSpacing: 2),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+
+                      // login gg/fb
+                      Text(
+                        'Hoặc đăng nhập với',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: const Color.fromARGB(190, 0, 0, 0),
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: 110,
+                                height: 65,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  // border: Border.all(color: Colors.black38),
+                                  borderRadius: BorderRadius.circular(5),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 0.5,
+                                      blurRadius: 8,
+                                      offset: Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+                                child: Image(
+                                    image: AssetImage('assets/images/gg.png')),
+                              ),
+                              Container(
+                                width: 110,
+                                height: 65,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  // border: Border.all(color: Colors.blue),
+                                  borderRadius: BorderRadius.circular(5),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 0.5,
+                                      blurRadius: 8,
+                                      offset: Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+                                child: Image(
+                                    image: AssetImage('assets/images/fb.png')),
+                              ),
+                            ]),
+                      ),
+
+                      // sign up
+                      SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Chưa có tài khoản? ',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: const Color.fromARGB(190, 0, 0, 0),
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 0,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUpScreen()));
+                            },
+                            child: Text(
+                              'Đăng ký',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
