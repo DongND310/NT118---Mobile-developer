9da6717cc0529ec6131164701f1aab1f9d6df743
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-04-24 20:21:01 +0700

    update BE (finish)

--- mobile_project/lib/screen/login-regis/forgotpass.dart
+++ mobile_project/lib/screen/login-regis/forgotpass.dart
@@ -1,13 +1,84 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile_project/components/inputtext.dart';
 import 'package:mobile_project/components/button.dart';
-
 import 'package:flutter/material.dart';
-import 'verifyemail.dart';
+import 'package:mobile_project/screen/login-regis/login.dart';
 
-class ForgotPassScreen extends StatelessWidget {
+class ForgotPassScreen extends StatefulWidget {
   ForgotPassScreen({super.key});
-  final usernameController = TextEditingController();
+
+  @override
+  State<ForgotPassScreen> createState() => _ForgotPassScreenState();
+}
+
+class _ForgotPassScreenState extends State<ForgotPassScreen> {
+  final emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
+  Future forgotPassword() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text.trim());
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text(
+              'Link reset mật khẩu đã được gửi đến email ' +
+                  emailController.text.trim() +
+                  '. Hãy kiểm tra email và thay đổi mật khẩu.',
+              style: TextStyle(fontSize: 15),
+            ),
+            actions: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "XÁC NHẬN",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          letterSpacing: 2),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    } on FirebaseAuthException catch (e) {
+      // Navigator.pop(context);
+      wrongMessage(e.code);
+    }
+  }
+
+  void wrongMessage(String errorMessage) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text(errorMessage),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +104,7 @@ class ForgotPassScreen extends StatelessWidget {
         child: ListView(
             padding: const EdgeInsets.only(top: 0.0, left: 40, right: 45),
             children: [
-              Text(
+              const Text(
                 'Quên mật khẩu',
                 style: TextStyle(
                   fontSize: 40,
@@ -42,7 +113,7 @@ class ForgotPassScreen extends StatelessWidget {
                   letterSpacing: 2,
                 ),
               ),
-              Text(
+              const Text(
                 'Nhập địa chỉ email để nhận mã xác thực',
                 style: TextStyle(
                   fontSize: 18,
@@ -57,21 +128,21 @@ class ForgotPassScreen extends StatelessWidget {
                   width: double.infinity,
                   child: Column(
                     children: [
-                      SizedBox(height: 20),
+                      const SizedBox(height: 20),
                       //Email, password
 
                       MyTextField(
-                          controller: usernameController,
+                          controller: emailController,
                           label: "Email",
                           hint: "Nhập email người dùng",
                           obscureText: false),
-                      SizedBox(
+                      const SizedBox(
                         height: 35,
                       ),
 
                       // button
                       MyButton(
-                        onTap: forgotPassword(),
+                        onTap: forgotPassword,
                         text: "TIẾP TỤC",
                       ),
                     ],
@@ -82,6 +153,4 @@ class ForgotPassScreen extends StatelessWidget {
       ),
     );
   }
-
-  forgotPassword() {}
 }
