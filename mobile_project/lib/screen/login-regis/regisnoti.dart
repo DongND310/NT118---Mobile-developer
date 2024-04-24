9c4e34f704dc4f491ab6ee8b6402d921c53e5a07
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-04-25 01:57:52 +0700

    update (done)

--- mobile_project/lib/screen/login-regis/regisnoti.dart
+++ mobile_project/lib/screen/login-regis/regisnoti.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_project/screen/homepage/homepage.dart';
-import 'package:mobile_project/components/button.dart';
 
 class RegisterSuccessScreen extends StatelessWidget {
   const RegisterSuccessScreen({super.key});
@@ -31,15 +30,15 @@ class RegisterSuccessScreen extends StatelessWidget {
           padding: const EdgeInsets.only(top: 0.0, left: 40, right: 45),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            SizedBox(
+            const SizedBox(
               height: 100,
             ),
             SvgPicture.asset(
                 'assets/icons/Check-Circle--Streamline-Core.svg.svg'),
-            SizedBox(
+            const SizedBox(
               height: 50,
             ),
-            Text(
+            const Text(
               'Thành công',
               style: TextStyle(
                 fontSize: 40,
@@ -48,10 +47,10 @@ class RegisterSuccessScreen extends StatelessWidget {
                 letterSpacing: 2,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 10,
             ),
-            Text(
+            const Text(
               'Tài khoản của bạn đã được tạo mới thành công. Đến trang chủ để khám phá thêm nhiều điều hấp dẫn!',
               textAlign: TextAlign.center,
               style: TextStyle(
@@ -61,19 +60,34 @@ class RegisterSuccessScreen extends StatelessWidget {
                 letterSpacing: 0,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 40,
             ),
             // button
-            MyButton(
-              onTap: SuccessRegis,
-              text: "ĐẾN TRANG CHỦ",
-            ),
+            GestureDetector(
+              onTap: () => Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => HomePageScreen())),
+              child: Container(
+                height: 55,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const Center(
+                  child: Text(
+                    "ĐẾN TRANG CHỦ",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 2),
+                  ),
+                ),
+              ),
+            )
           ]),
         ),
       ),
     );
   }
-
-  SuccessRegis() {}
 }
