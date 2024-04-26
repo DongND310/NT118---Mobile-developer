68c606813a59481a35ffa72bce1535de2cae1bba
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-04-26 18:00:56 +0700

    update

--- mobile_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,4 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
