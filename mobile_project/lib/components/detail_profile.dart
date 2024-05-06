7a9e4d5e0fc417c4ad165bcdf61eef5031fe2c98
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-05-06 18:25:15 +0700

    update profile detail

---
+++ mobile_project/lib/components/detail_profile.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class UserDetailInfo extends StatelessWidget {
+  final String text;
+  final String change;
+  const UserDetailInfo({
+    super.key,
+    required this.text,
+    required this.change,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          text ?? '',
+          style: TextStyle(color: Colors.black, fontSize: 18),
+        ),
+        Row(
+          children: [
+            Text(
+              change,
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+            ),
+            IconButton(
+              onPressed: () {
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => PersonDetailProfileScreen()));
+              },
+              icon: const Icon(
+                Icons.arrow_forward_ios_sharp,
+                color: Colors.grey,
+                size: 20,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
