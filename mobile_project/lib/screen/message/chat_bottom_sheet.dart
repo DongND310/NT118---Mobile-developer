1c503c938388896fc491b15928e161b1d97bd08c
Nguyen Duy Dong <21521956@gm.uit.edu.vn>
2024-05-23 00:49:24 +0700

    update fe

--- mobile_project/lib/screen/message/chat_bottom_sheet.dart
+++ mobile_project/lib/screen/message/chat_bottom_sheet.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../services/chat_service.dart';
 
@@ -15,10 +15,14 @@ class ChatBottomSheet extends StatelessWidget {
       required this.receiverName,
       required this.chatterImg});
 
-  void onSendMessage()  {
+  void onSendMessage() {
     if (_controller.text.isNotEmpty) {
-       _chatService.sendMessage(
-          receiverId, receiverName,  _controller.text, chatterImg,);
+      _chatService.sendMessage(
+        receiverId,
+        receiverName,
+        _controller.text,
+        chatterImg,
+      );
       _controller.clear();
     }
   }
@@ -51,16 +55,18 @@ class ChatBottomSheet extends StatelessWidget {
           Expanded(
             child: Container(
               alignment: Alignment.centerRight,
-              width: 250,
               child: TextFormField(
                 cursorColor: Colors.blue,
                 controller: _controller,
+                maxLines: 10,
+                minLines: 1,
                 decoration: const InputDecoration(
                     hintText: "Hãy nhắn gì đi...", border: InputBorder.none),
               ),
             ),
           ),
-          Expanded(
+          Padding(
+            padding: const EdgeInsets.only(top: 0.0, left: 10, right: 10),
             child: Align(
               alignment: Alignment.centerRight,
               child: IconButton(
